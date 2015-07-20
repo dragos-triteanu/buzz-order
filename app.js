1da3bfc9a0385c8239e754c289d9639165dc17e6
dragos.triteanu <dragos.triteanu@gmail.com>
2015-07-20 09:33:01 +0300

    Added Logger middleware example

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var port = 3000;
 
 var SomeModel = require('./model/SomeModel');
 var db = require("./db/DB");
+var logger = require('./logger/Logger');
+var homeController = require("./controller/HomeController")(express);
 
-//Indicated to node that 'resources/views' will be used as folder for using static html.
 /**
  * Configurations
  */
+//Indicated to node that 'resources/views' will be used as folder for using static html.
 app.use(express.static('resources/views'));
 app.use(express.static('resources'));
 
-var homeController = require("./controller/HomeController")(express);
-
+app.use(logger);
 app.use("/",homeController);
 
 
