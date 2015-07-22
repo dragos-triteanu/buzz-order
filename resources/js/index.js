0ff674fab17ad7ba5ff67a77c6751f8e929f1718
Dragos Triteanu <dragos.triteanu@gmail.com>
2015-07-22 23:58:38 +0300

    Continued with angular tutorial

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,9 @@
 (function(){
     var app = angular.module('home',[]);
-    var products = [{name:"SomeProduct",value:27,visible:true},
-                    {name:"AnotherProduct",value:25,visible:true}];
+    var products = [{name:"SomeProduct",price:27,visible:true,description:"ceva",image:"images/image1.jpg"},
+                    {name:"AnotherProduct",price:25,visible:true,description:"altceva",image:"images/image2.jpg"}];
 
-    app.controller('HomeController',function(){
+    app.controller('UserController',function(){
         this.products = products;
     });
 
